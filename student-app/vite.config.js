983fda8ad8bf5ad9f2330a656693cbc3b606874d
Erick Chavez <erickchavezescobar@gmail.com>
2025-03-02 23:41:39 -0600

    dist fold created with npm run build

--- student-app/vite.config.js
+++ student-app/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'
+            }
+            if (id.includes('lodash')) {
+              return 'lodash-vendor'
+            }
+            return 'vendor' // Create a separate "vendor" chunk for large dependencies
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // (Optional) Adjust warning limit if necessary
+  },
 })
